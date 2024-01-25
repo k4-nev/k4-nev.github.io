74357c83fc7b9d0ee5fece10772e1e58435e5166
Daniil <127127222+cap-shveps@users.noreply.github.com>
2024-01-25 20:51:17 +0400

    newJS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,36 @@
 /* Основной файл скрипта JS */
-const chevron = document.querySelector('.nav__chevron'),
-    close = document.querySelector('.nav__chevron--active'),
-    sidebar = document.querySelector('.sidebar'),
-    overlay = document.querySelector('.overlay'),
+'use strict';
 
-    percent = document.querySelectorAll('.skills__percent'),
-    lines = document.querySelectorAll('.skills__line span');
+window.addEventListener('DOMContentLoaded', () => {
+	const chevron = document.querySelector('.nav__chevron'),
+		close = document.querySelector('.nav__chevron--active'),
+		sidebar = document.querySelector('.sidebar'),
+		overlay = document.querySelector('.overlay'),
+		percent = document.querySelectorAll('.skills__percent'),
+		lines = document.querySelectorAll('.skills__line span'),
+		navLink = document.querySelectorAll('.nav__list a');
 
-chevron.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar--active'),
-    chevron.classList.toggle('nav__chevron--active');
-    overlay.classList.toggle('overlay--active');
+	chevron.addEventListener('click', () => {
+		sidebar.classList.toggle('sidebar--active');
+		chevron.classList.toggle('nav__chevron--active');
+		overlay.classList.toggle('overlay--active');
+	});
+
+	overlay.addEventListener('click', () => {
+		sidebar.classList.toggle('sidebar--active');
+		chevron.classList.toggle('nav__chevron--active');
+		overlay.classList.toggle('overlay--active');
+	});
+
+	navLink.forEach((item) => {
+		if (item.href === window.location.pathname || item.href === window.location.href) {
+			item.className += ' nav__link--active';
+		} else if (window.location.pathname == '/') {
+			navLink[0].className += ' nav__link--active';
+		}
+	})
+
+	percent.forEach((item, i) => {
+		lines[i].style.width = item.innerHTML;
+	});
 });
-
-overlay.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar--active'),
-    chevron.classList.toggle('nav__chevron--active');
-    overlay.classList.toggle('overlay--active');
-});
-
-(function (){
-    var a = document.querySelectorAll('.nav__list a');
-      for (var i=a.length; i--;) {
-        if (a[i].href === window.location.pathname || a[i].href === window.location.href) a[i].className += ' nav__link--active';
-      }
-  })();
-
-percent.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
-});
-
